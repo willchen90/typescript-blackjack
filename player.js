9e2c77b964cc79c425481349183237abedc58d13
willchen90 <willchen90@gmail.com>
2015-11-28 15:57:21 -0800

    remove gitignored files

--- player.js
+++
@@ -1,7 +0,0 @@
-var Player;
-(function (Player) {
-    Player[Player["Human"] = 0] = "Human";
-    Player[Player["Computer"] = 1] = "Computer";
-})(Player || (Player = {}));
-exports.default = Player;
-//# sourceMappingURL=player.js.map
--- suit.js
+++
@@ -1,9 +0,0 @@
-var Suit;
-(function (Suit) {
-    Suit[Suit["Clubs"] = 0] = "Clubs";
-    Suit[Suit["Diamonds"] = 1] = "Diamonds";
-    Suit[Suit["Hearts"] = 2] = "Hearts";
-    Suit[Suit["Spades"] = 3] = "Spades";
-})(Suit || (Suit = {}));
-exports.default = Suit;
-//# sourceMappingURL=suit.js.map
